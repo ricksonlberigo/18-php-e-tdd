abbb8f8d29055b18e504bb5dbf89fbf1e1364b3f
ricksonl.berigo <ricksonl.berigo@outlook.com>
2023-06-05 15:43:03 -0300

    Implemento menor valor no nosso avaliador, e adicionando mais testes

--- tests/Service/AvaliadorTest.php
+++ tests/Service/AvaliadorTest.php
@@ -51,4 +51,46 @@ class AvaliadorTest extends TestCase
     // Assert - Then
     self::assertEquals(1000000, $maiorValor);
   }
+
+  public function testAvaliadorDeveEncontrarOMenorValorDeLancesEmOrdemDecrescente()
+  {
+    // Arrange - Given
+    $leilao = new Leilao("BMW Audi A4 0KM");
+
+    $rickson = new Usuario("Rickson Lucas");
+    $kamilly = new Usuario("Kamilly Vitoria");
+
+    $leilao->recebeLance(new Lance($rickson, 1000000));
+    $leilao->recebeLance(new Lance($kamilly, 900000));
+
+    $leiloeiro = new Avaliador();
+
+    // Act - When
+    $leiloeiro->avalia($leilao);
+    $menorValor = $leiloeiro->getMenorValor();
+
+    // Assert - Then
+    self::assertEquals(900000, $menorValor);
+  }
+
+  public function testAvaliadorDeveEncontrarOMenorValorDeLancesEmOrdemCrescente()
+  {
+    // Arrange - Given
+    $leilao = new Leilao("BMW Audi A4 0KM");
+
+    $rickson = new Usuario("Rickson Lucas");
+    $kamilly = new Usuario("Kamilly Vitoria");
+
+    $leilao->recebeLance(new Lance($kamilly, 900000));
+    $leilao->recebeLance(new Lance($rickson, 1000000));
+
+    $leiloeiro = new Avaliador();
+
+    // Act - When
+    $leiloeiro->avalia($leilao);
+    $menorValor = $leiloeiro->getMenorValor();
+
+    // Assert - Then
+    self::assertEquals(900000, $menorValor);
+  }
 }
